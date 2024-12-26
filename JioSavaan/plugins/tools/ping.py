2b8ac25b09ced7f78c7799cd0068c6a269b67297
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 06:58:45 +0530

    Add files via upload

---
+++ JioSavaan/plugins/tools/ping.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from Jiosaavn import app
+from Jiosaavn.core.call import Anony
+from Jiosaavn.utils import bot_sys_stats
+from Jiosaavn.utils.decorators.language import language
+from Jiosaavn.utils.inline import supp_markup
+from config import BANNED_USERS, PING_IMG_URL
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Anony.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
