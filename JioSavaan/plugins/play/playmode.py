7d04e56be44c5171be3f83174596cf04afc3d496
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:26:55 +0530

    Update playmode.py

--- JioSavaan/plugins/play/playmode.py
+++ JioSavaan/plugins/play/playmode.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
-from AnonXMusic.utils.decorators import language
-from AnonXMusic.utils.inline.settings import playmode_users_markup
+from JioSavaan import app
+from JioSavaan.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from JioSavaan.utils.decorators import language
+from JioSavaan.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
 
 
