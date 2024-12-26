1832690fcf85553a04101ea01c53d79a2a61fdcc
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:36:21 +0530

    Update shuffle.py

--- JioSavaan/plugins/admins/shuffle.py
+++ JioSavaan/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from JioSavaan import app
+from JioSavaan.misc import db
+from JioSavaan.utils.decorators import AdminRightsCheck
+from JioSavaan.utils.inline import close_markup
 from config import BANNED_USERS
 
 
