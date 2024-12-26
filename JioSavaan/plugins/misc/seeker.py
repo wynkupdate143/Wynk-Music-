4471080eaaff888e0f42711c6c5f09747a56c784
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:55:06 +0530

    Update seeker.py

--- JioSavaan/plugins/misc/seeker.py
+++ JioSavaan/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_active_chats, is_music_playing
+from JioSavaan.misc import db
+from JioSavaan.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
