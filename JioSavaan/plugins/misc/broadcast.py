fb139991711aabad96a72a68e850137aaff61795
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 09:15:08 +0530

    Update broadcast.py

--- JioSavaan/plugins/misc/broadcast.py
+++ JioSavaan/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command("srajputbroadcast") & SUDOERS)
+@app.on_message(filters.command("broadcast") & SUDOERS)
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
