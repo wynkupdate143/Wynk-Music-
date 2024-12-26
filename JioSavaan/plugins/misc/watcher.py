c42dd632b5614a97c862ae5bdcf928d1561ffd9e
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:55:23 +0530

    Update watcher.py

--- JioSavaan/plugins/misc/watcher.py
+++ JioSavaan/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from JioSavaan import app
+from JioSavaan.core.call import Anony
 
 welcome = 20
 close = 30
