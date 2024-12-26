89ceccda4a7df4063e20ad940f5cfa5b77d74e9a
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 07:55:52 +0530

    Update __init__.py

--- JioSavaan/__init__.py
+++ JioSavaan/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from JioSavaan.core.bot import Anony
+from JioSavaan.core.dir import dirr
+from JioSavaan.core.git import git
+from JioSavaan.core.userbot import Userbot
+from JioSavaan.misc import dbb, heroku
 
 from .logging import LOGGER
 
