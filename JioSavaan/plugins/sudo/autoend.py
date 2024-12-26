dc4fac72cd223f50e30f2a1337ce99335a6263f3
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:26:14 +0530

    Update autoend.py

--- JioSavaan/plugins/sudo/autoend.py
+++ JioSavaan/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off, autoend_on
+from JioSavaan import app
+from JioSavaan.misc import SUDOERS
+from JioSavaan.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
