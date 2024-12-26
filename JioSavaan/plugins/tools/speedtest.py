4d11ab38322e6b2945eca6ad6e13ed7c8b8d80c4
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:18:20 +0530

    Update speedtest.py

--- JioSavaan/plugins/tools/speedtest.py
+++ JioSavaan/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.decorators.language import language
+from JioSavaan import app
+from JioSavaan.misc import SUDOERS
+from JioSavaan.utils.decorators.language import language
 
 
 def testspeed(m, _):
