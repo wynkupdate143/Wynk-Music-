a33e6df7c113032199b262f1029e548df4e5249e
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:27:52 +0530

    Update play.py

--- JioSavaan/plugins/play/play.py
+++ JioSavaan/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from JioSavaan import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from JioSavaan.core.call import Anony
+from JioSavaan.utils import seconds_to_min, time_to_seconds
+from JioSavaan.utils.channelplay import get_channeplayCB
+from JioSavaan.utils.decorators.language import languageCB
+from JioSavaan.utils.decorators.play import PlayWrapper
+from JioSavaan.utils.formatters import formats
+from JioSavaan.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from JioSavaan.utils.logger import play_logs
+from JioSavaan.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
