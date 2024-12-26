0433a0ff213fcf375e6aaa3384f323a332183ce9
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:00:44 +0530

    Update database.py

--- JioSavaan/utils/database.py
+++ JioSavaan/utils/database.py
@@ -3,8 +3,8 @@ import asyncio
 from datetime import date
 from typing import Dict, List, Union
 
-from AnonXMusic import userbot
-from AnonXMusic.core.mongo import mongodb
+from JioSavaan import userbot
+from JioSavaan.core.mongo import mongodb
 
 authdb = mongodb.adminauth
 authuserdb = mongodb.authuser
@@ -72,7 +72,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from JioSavaan.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -86,7 +86,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from AnonXMusic.core.userbot import assistants
+    from JioSavaan.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -113,7 +113,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from AnonXMusic.core.userbot import assistants
+    from JioSavaan.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -126,7 +126,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from AnonXMusic.core.userbot import assistants
+    from JioSavaan.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
