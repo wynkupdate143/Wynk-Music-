9cbffeef2033a959c1bae4ad91f4d11f3bd5f6db
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:03:29 +0530

    Update stream.py

--- JioSavaan/utils/stream/stream.py
+++ JioSavaan/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from JioSavaan import Carbon, YouTube, app
+from JioSavaan.core.call import Anony
+from JioSavaan.misc import db
+from JioSavaan.utils.database import add_active_video_chat, is_active_chat
+from JioSavaan.utils.exceptions import AssistantErr
+from JioSavaan.utils.inline import aq_markup, close_markup, stream_markup
+from JioSavaan.utils.pastebin import AnonyBin
+from JioSavaan.utils.stream.queue import put_queue, put_queue_index
+from JioSavaan.utils.thumbnails import get_thumb
 
 
 async def stream(
