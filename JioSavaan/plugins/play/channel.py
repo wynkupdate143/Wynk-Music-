9be72f05db7579b4b32feb65f1b65f1adf7f78cf
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:29:01 +0530

    Update channel.py

--- JioSavaan/plugins/play/channel.py
+++ JioSavaan/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import set_cmode
-from AnonXMusic.utils.decorators.admins import AdminActual
+from JioSavaan import app
+from JioSavaan.utils.database import set_cmode
+from JioSavaan.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
