6222a9ecb284814da946290727e51971b9cfb089
Jio Saavan <asrushfig@gmail.com>
2024-12-26 17:12:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://www.jiosaavn.com/",
+    "https://github.com/jiosaavnmusic/JioSaavn",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
