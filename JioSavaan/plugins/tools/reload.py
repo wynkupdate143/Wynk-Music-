4ec31faa33ebd65f1d20a08eebc457a731107777
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:19:01 +0530

    Update reload.py

--- JioSavaan/plugins/tools/reload.py
+++ JioSavaan/plugins/tools/reload.py
@@ -5,12 +5,12 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
-from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
+from JioSavaan import app
+from JioSavaan.core.call import Anony
+from JioSavaan.misc import db
+from JioSavaan.utils.database import get_assistant, get_authuser_names, get_cmode
+from JioSavaan.utils.decorators import ActualAdminCB, AdminActual, language
+from JioSavaan.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
 rel = {}
