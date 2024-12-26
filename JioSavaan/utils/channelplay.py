31ff41e9a59cd462d11cdcb3296cfd907d8419b0
jiosaavnmusic <asrushfig@gmail.com>
2024-12-26 08:02:24 +0530

    Update channelplay.py

--- JioSavaan/utils/channelplay.py
+++ JioSavaan/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from JioSavaan import app
+from JioSavaan.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
