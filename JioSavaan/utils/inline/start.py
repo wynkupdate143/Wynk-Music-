4c1188e4f2772a5eff91f1e96052446458542620
wynkupdate143 <amansingh093789@gmail.com>
2025-06-13 00:39:00 +0530

    Update start.py

--- JioSavaan/utils/inline/start.py
+++ JioSavaan/utils/inline/start.py
@@ -16,7 +16,6 @@ def start_panel(_):
     ]
     return buttons
 
-
 def private_panel(_):
     buttons = [
         [
@@ -24,15 +23,5 @@ def private_panel(_):
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
-        ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
-        ],
-    ]
+        ],[InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")], ]
     return buttons
